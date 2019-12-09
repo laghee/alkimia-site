1f55178821389003f0861e9bb1a4c561512855df
laghee <katrua@gmail.com>
2019-12-09 17:56:55 +0000

    Initial commit

---
+++ staging.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask app for you to get started...
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def benvenuti():
+    return render_template('index.html')
+
+# @app.route("/form", methods=["GET", "POST"])
+# def my_form():
+#     if request.method == 'POST':
+#         reply_to = request.form.get('email')
+#         message = request.form.get('message')
+#         # send_email(message, reply_to)
+#     return render_template('my_form.html')
